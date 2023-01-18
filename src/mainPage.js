b02cb884e458f0a44aac7973f0511a67dc20c014
Sree Dev <sreedev0115@gmail.com>
2023-01-18 13:51:40 +0530

    Loader Screen Added

---
+++ src/mainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function mainPage(){
+    return(
+        <div align = 'center'>
+            <p>Main Content here !</p>
+        </div>
+    )
+}
+
+export default mainPage
